0af423244e95ef1d10a1ea78b34e36a020d27c65
Lorena Medeiros <lomedeiros@proton.me>
2024-07-24 17:19:03 -0300

    feat: add controller advice

---
+++ src/main/java/io/github/lsmcodes/gof/exception/GofApplicationExceptionHandler.java
@@ -0,0 +1,30 @@
+package io.github.lsmcodes.gof.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.ServerErrorException;
+
+import io.github.lsmcodes.gof.dto.response.Response;
+
+@ControllerAdvice
+public class GofApplicationExceptionHandler<T> {
+        
+        @ExceptionHandler(value = { ClientNotFoundException.class })
+        public ResponseEntity<Response<T>> handleClientNotFoundException(ClientNotFoundException exception) {
+                Response<T> response = new Response<>();
+                response.addErrorsToResponse(404, exception.getLocalizedMessage());
+
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+        }
+
+        @ExceptionHandler(value = { ServerErrorException.class })
+        public ResponseEntity<Response<T>> handleServerErrorException(ServerErrorException exception) {
+                Response<T> response = new Response<>();
+                response.addErrorsToResponse(500, exception.getMessage());
+
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+        }
+
+}
