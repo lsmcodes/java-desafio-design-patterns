3d60caf701f7125028559855b90878b1501f5d38
Lorena Medeiros <lomedeiros@proton.me>
2024-07-24 14:46:36 -0300

    feat: create custom exception ClientNotFoundException

---
+++ src/main/java/io/github/lsmcodes/gof/exception/ClientNotFoundException.java
@@ -0,0 +1,13 @@
+package io.github.lsmcodes.gof.exception;
+
+public class ClientNotFoundException extends Exception {
+
+        public ClientNotFoundException() {
+                super();
+        }
+
+        public ClientNotFoundException(String message) {
+                super(message);
+        }
+
+}
